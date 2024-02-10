18b712d6bb4643c47006e5c8420a20d28d7b060d
George Park <gminator@gmail.com>
2024-02-09 16:07:15 -0800

    my changes

--- Desktop/Code/Top150Interiew/RomanToInteger.js
+++ Desktop/Code/Top150Interiew/RomanToInteger.js
@@ -17,28 +17,38 @@
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
 
- 
-
 // Example 1:
-
 // Input: s = "III"
 // Output: 3
 // Explanation: III = 3.
-// Example 2:
 
+// Example 2:
 // Input: s = "LVIII"
 // Output: 58
 // Explanation: L = 50, V= 5, III = 3.
-// Example 3:
 
+// Example 3:
 // Input: s = "MCMXCIV"
 // Output: 1994
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
-
+å
 /**
  * @param {string} s
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    let roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+
+
 };
+
+console.log(romanToInt(II))
