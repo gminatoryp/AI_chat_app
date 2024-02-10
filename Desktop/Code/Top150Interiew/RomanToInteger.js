906c7e7f4ac9189ad9b7da5658735d8c7b7f5f55
George Park <gminator@gmail.com>
2024-02-09 16:26:44 -0800

    roman to int solution 2

--- Desktop/Code/Top150Interiew/RomanToInteger.js
+++ Desktop/Code/Top150Interiew/RomanToInteger.js
@@ -116,3 +116,29 @@ for (var i = s.length - 1; i >= 0; i--) {
 }
 
 return result;
+
+// solution
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var sym = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
+}
+
+var romanToInt = function(s) {
+  var result = 0;
+  for(var i = 0; i < s.length; i++) {
+    result += sym[s[i]] * (sym[s[i]] < sym[s[i+1]] ? -1 : 1)
+  }
+  
+  return result;
+};
