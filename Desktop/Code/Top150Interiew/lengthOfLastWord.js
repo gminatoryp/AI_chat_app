bd41452dee35bd73d20ff5d896d118902d55f088
George Park <gminator@gmail.com>
2024-02-19 19:29:54 -0800

    length of last word solution

--- Desktop/Code/Top150Interiew/lengthOfLastWord.js
+++ Desktop/Code/Top150Interiew/lengthOfLastWord.js
@@ -4,20 +4,18 @@
 // substring
 //  consisting of non-space characters only.
 
- 
-
 // Example 1:
 
 // Input: s = "Hello World"
 // Output: 5
 // Explanation: The last word is "World" with length 5.
-// Example 2:
 
+// Example 2:
 // Input: s = "   fly me   to   the moon  "
 // Output: 4
 // Explanation: The last word is "moon" with length 4.
-// Example 3:
 
+// Example 3:
 // Input: s = "luffy is still joyboy"
 // Output: 6
 // Explanation: The last word is "joyboy" with length 6.
@@ -27,5 +25,14 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+    let lastWordLength = []
+    for(let i = s.length - 1 ; i >= 0; i--) {
+        if(s[i] !== ' ') {
+            lastWordLength++
+        } else {
+            return lastWordLength
+        }
+    }
 };
+
+console.log(lengthOfLastWord('hello world'))
