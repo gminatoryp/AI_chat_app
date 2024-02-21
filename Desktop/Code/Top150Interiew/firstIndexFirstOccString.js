f9019f1d388c3c0550fa11f8b2c1993f12348ce4
George Park <gminator@gmail.com>
2024-02-20 20:02:59 -0800

    haystack solution 3

--- Desktop/Code/Top150Interiew/firstIndexFirstOccString.js
+++ Desktop/Code/Top150Interiew/firstIndexFirstOccString.js
@@ -46,3 +46,14 @@ var strStr = function(haystack, needle) {
     return haystack.includes(needle) ? haystack.indexOf(needle) : -1;
 };
 };
+
+//solution 3
+var strStr = function(haystack, needle) {
+  if (!needle) return 0
+
+  for (let i = 0 ; i < haystack.length ; i++) {
+    if (haystack.slice(i, i+needle.length) === needle) return i
+  }
+
+  return -1
+}
