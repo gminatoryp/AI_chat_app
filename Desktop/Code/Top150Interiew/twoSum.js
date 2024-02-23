068d68f69fd65b975096b340472a6f588d266a51
George Park <gminator@gmail.com>
2024-02-22 20:05:23 -0800

    two sum solution

--- Desktop/Code/Top150Interiew/twoSum.js
+++ Desktop/Code/Top150Interiew/twoSum.js
@@ -22,4 +22,41 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+  let newArray = [];
+
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] + nums[i + 1] === target) {
+      newArray.push(nums[i]);
+    }
+  }
+};
+return newArray;
+
+console.log(twoSum([1, 2, 3, 4], 5));
+
+//solution
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const numToIndexMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numToIndexMap.has(complement)) {
+      return [numToIndexMap.get(complement), i];
+    }
+    numToIndexMap.set(nums[i], i);
+  }
+
+  return []; // No solution found
+};
+
+// Example usage:
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target)); // Output: [0, 1]
